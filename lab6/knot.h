bfba550fb842f9bc2df898ee56b0bd439e53fc49
Amelia <47516546+tainella@users.noreply.github.com>
2020-03-29 23:27:50 +0300

    Create knot.h

---
+++ lab6/knot.h
@@ -0,0 +1,49 @@
+#ifndef KNOT_H
+#define KNOT_H
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <process.h>  
+#include "vocabulary.h"
+
+int BEGIN_PORT = 30000;
+const char* BIND_URL = "tcp://*:";
+const char* CON_URL = "tcp://localhost:";
+
+char* BindURLPort(int port);
+
+char* ConURLPort(int port);
+
+int TakePort(zmq::socket_t& socket);
+
+typedef struct _n {
+	int id;
+    int id_l;
+    int id_r;
+    void* context_me;
+    void* r_me;
+    pid_t pid;
+    voc* v;
+    int port_fl;
+    void* context_fl;
+    void* r_fl;
+    int port_fr;
+    void* context_fr;
+    void* r_fr;
+} knot;
+
+knot* knot_create(void* id, void* pport);
+knot* knot_add(knot* k, int id);
+void knot_find(knot* k, int id);
+void knot_destroy(knot* k);
+
+#endif
