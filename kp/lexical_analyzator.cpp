963731e5e8db4395222428374ffb7281661f7cfd
Amelia <47516546+tainella@users.noreply.github.com>
2020-07-29 18:42:34 +0300

    Update lexical_analyzator.cpp

--- kp/lexical_analyzator.cpp
+++ kp/lexical_analyzator.cpp
@@ -7,8 +7,8 @@
 #include <sys/uio.h>
 #include <unistd.h>
 
-enum Lexeme {
-    IDENTIFIER, KEYWORD, OPERATOR, LITERAL, SEPARATOR
+enum Lexeme { //препроцессорные лексемы
+    CONST, NAME, IDENTIFIER, KEYWORD, OPERATOR, NUMBER, SEPARATOR
 };
 
 class token {
@@ -22,18 +22,70 @@ public:
 };
 
 int main(int argc, const char * argv[]) { //ввод имя файла
-    int fp;
+    int fp,fl, state = -1;
     ssize_t n;
     char c;
     char com[20];
     com[0] = '\0';
+    
     if ((fp = open(argv[1], O_RDWR)) < 0) {
         printf("Cannot open file.\n");
         exit(1);
     }
+    if ((fl = creat("lprework.txt", 0666)) == -1) { //буфер для редактирования
+        printf("Cannot create file.\n");
+        exit(1);
+    }
+    /* список состояний:
+     -1 обработка не символов для препроцессора
+     0 обработка директивы
+     1 удаляем комментарии пока встречаемый символ не \0
+     2 удаляем комментарии пока не встретится * /
+     3 подозрение на коментарий
+     4 подозрение на закрытие комментария
+     
+     */
     while (1) {
         if((n = read(fp,&c, 1)) > 0) {
-            
+            switch (state)
+            {
+                case -1:
+                    if (c == '#') {
+                        state = 0;
+                    }
+                    else if (c == '/') {
+                        state = 4;
+                    }
+                    if (write(fl, &c, n) != n)
+                        printf("Error in writing in.\n");
+                    break;
+                case 0:
+                    
+                    break;
+                case 1:
+                    if (c == '\0') {
+                        state = -1;
+                    }
+                    break;
+                case 2:
+                    if (c == '*') {
+                        state = 4;
+                    }
+                    break;
+                case 3:
+                    if (c == '/') {
+                        state = 1;
+                    }
+                    else if (c == '*') {
+                        state = 2;
+                    }
+                    break;
+                case 4:
+                    if (c == '/') {
+                        state = -1;
+                    }
+                    break;
+            };
         }
         else {
             close(fp);
