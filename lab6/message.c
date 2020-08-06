177c4f0a1054f465fe06c8f6e409f11578d8e5cf
Amelia <47516546+tainella@users.noreply.github.com>
2020-08-07 00:05:01 +0300

    Update message.c

--- lab6/message.c
+++ lab6/message.c
@@ -6,7 +6,6 @@ void send(void* r, char* action, int id, char* pat, ID nam, int value) {
     zmq_msg_t pathMes;
     zmq_msg_t nameMes;
     zmq_msg_t valueMes;
-    //zmq_msg_t Dop;
     
     char str1[10];
     sprintf(str1, "%d", id);
@@ -19,9 +18,7 @@ void send(void* r, char* action, int id, char* pat, ID nam, int value) {
 	zmq_msg_init_data (&pathMes, pat, sizeof(pat), NULL, NULL);
 	zmq_msg_init_data (&nameMes, nam,sizeof(nam), NULL, NULL);
 	zmq_msg_init_data (&valueMes, str,sizeof(str), NULL, NULL);
-	//zmq_msg_init_data (&Dop, "dop", sizeof("dop"), NULL, NULL);
 	
-	printf("Пытаюсь отправить сообщение, action:%s id:%d pat:%s|, pathMes %s|, nameMes %s|\n",action, id, pat,(char*)zmq_msg_data(&pathMes), (char*)zmq_msg_data(&nameMes));
 	//ZMQ_SNDMORE
 	sleep(2);
 	zmq_msg_send(&actionMes, r, ZMQ_SNDMORE);
@@ -34,15 +31,12 @@ void send(void* r, char* action, int id, char* pat, ID nam, int value) {
 	sleep(1);
 	zmq_msg_send(&valueMes, r, 0);
 	sleep(1);
-	//zmq_msg_send(&Dop, r, 0);
-	sleep(1);
 	
 	zmq_msg_close(&actionMes);
 	zmq_msg_close(&idMes);
 	zmq_msg_close(&pathMes);
 	zmq_msg_close(&nameMes);
 	zmq_msg_close(&valueMes);
-	//zmq_msg_close(&Dop);
 }
 
 message* rec(void* r) {
@@ -52,17 +46,14 @@ message* rec(void* r) {
     zmq_msg_t pathMes;
     zmq_msg_t nameMes;
     zmq_msg_t valueMes;
-    //zmq_msg_t Dop;
     
     zmq_msg_init(&actionMes);
     zmq_msg_init(&idMes);
     zmq_msg_init(&pathMes);
     zmq_msg_init(&nameMes);
     zmq_msg_init(&valueMes);
-    //zmq_msg_init(&Dop);
     
     zmq_msg_recv(&actionMes, r, 0);
-    printf("ПОПЫТКА \n");
     int length = zmq_msg_size(&actionMes);
 	m->action = malloc(length);
 	memcpy(m->action, zmq_msg_data(&actionMes), length);
@@ -96,14 +87,11 @@ message* rec(void* r) {
     	i++;
     }
     m->value = k;
-    //zmq_msg_recv(&Dop, r, 0);
-    printf("ПОЛУЧЕНО: %s, %d, %s,  %s, %d\n", m->action, m->id, m->path, m->name, m->value);
     
     zmq_msg_close(&actionMes);
 	zmq_msg_close(&idMes);
 	zmq_msg_close(&pathMes);
 	zmq_msg_close(&nameMes);
 	zmq_msg_close(&valueMes);
-	//zmq_msg_close(&Dop);
 	return m;
 }
